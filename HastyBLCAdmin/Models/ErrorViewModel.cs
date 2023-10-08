0ec2d4af6872c4f1d5a0e7d8917e6e65a8ee13e2
aaronalcuitas <aaronalcuitas16@gmail.com>
2023-10-08 20:40:45 +0800

    initial setup

---
+++ HastyBLCAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HastyBLCAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
