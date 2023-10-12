b33a5450102121300101639bfe12f75e895c1de7
aaronalcuitas <aaronalcuitas16@gmail.com>
2023-10-13 00:58:12 +0800

    added initialcreate migration

---
+++ HastyBLC/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HastyBLC.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int GenreId { get; set; }
+        public string? Name { get; set; }
+        public ICollection<BookGenre>? BookGenres { get; set; }
+
+    }
+}
