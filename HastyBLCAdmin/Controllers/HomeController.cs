37efa81d5dbcf2a730fed7ae17c5af4df93936fc
Jillian <jillian.tenerife@usjr.edu.ph>
2023-10-28 00:43:55 +0800

    viewbook

--- HastyBLCAdmin/Controllers/HomeController.cs
+++ HastyBLCAdmin/Controllers/HomeController.cs
@@ -173,5 +173,41 @@ namespace HastyBLCAdmin.Controllers
                 return View(model);
             }
         }
+        public IActionResult ViewBook(int id)
+        {
+            var book = _context.Books
+                .Include(b => b.Author)
+                .Include(b => b.BookGenres)
+                .ThenInclude(bg => bg.Genre)
+                .FirstOrDefault(b => b.BookId == id);
+
+            if (book == null)
+            {
+                TempData["ErrorMessage"] = "Book not found.";
+                return RedirectToAction("Books");
+            }
+
+            var genreNames = book.BookGenres?.Select(bg => bg.Genre?.Name);
+            var concatGenre = string.Join(", ", genreNames!);
+
+            var bookViewModel = new Models.BookViewModel
+            {
+                Title = book.Title,
+                Description = book.Description,
+                Image = book.Image,
+                PublishDate = book.PublishDate,
+                Publisher = book.Publisher,
+                Isbn = book.Isbn,
+                Language = book.Language,
+                Format = book.Format,
+                Pages = book.Pages,
+                AuthorName = book.Author!.Name,
+                Genres = book.BookGenres!.Select(bookGenre => bookGenre.Genre!.Name).ToList(),
+            };
+
+            return View(bookViewModel);
+        }
+
+
     }
 }
