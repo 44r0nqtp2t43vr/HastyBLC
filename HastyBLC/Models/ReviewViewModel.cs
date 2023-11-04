53b7be63a286a43caaa3b6a49a7575abe4f5e44f
punseveII <ella.punay@usjr.edu.ph>
2023-11-04 10:46:21 +0800

    added validation in the review form

--- HastyBLC/Models/ReviewViewModel.cs
+++ HastyBLC/Models/ReviewViewModel.cs
@@ -1,10 +1,23 @@
-﻿namespace HastyBLC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HastyBLC.Models
 {
     public class ReviewViewModel
     {
         public int BookId { get; set; }
+
         public string? BookTitle { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Email Address is required.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Stars is required.")]
         public int Stars { get; set; }
+
+        [Required(ErrorMessage = "Comment is required.")]
         public string? Comment { get; set; }
     }
 }
