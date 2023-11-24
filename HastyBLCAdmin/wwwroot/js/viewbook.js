3aeb8f8a5d1c1f75029f36083307520a4a521b17
Jillian <jillian.tenerife@usjr.edu.ph>
2023-11-24 22:48:18 +0800

    viewbook changes

--- HastyBLCAdmin/wwwroot/js/viewbook.js
+++ HastyBLCAdmin/wwwroot/js/viewbook.js
@@ -1,20 +1,36 @@
 ﻿var jq = jQuery.noConflict();
 jq(document).ready(function () {
-    jq(".editReviewBtn").click(function () {
-        var reviewId = $(this).data("review-id");
-        var reviewName = $(this).data("review-name");
-        var reviewRating = $(this).data("review-rating");
-        var reviewUserEmail = $(this).data("review-user-email");
-        var reviewDescription = $(this).data("review-description");
+    
+    jq('.editReviewBtn').on('click', function () {
+        var reviewId = jq(this).data('review-id');
+        var name = jq(this).data('review-name');
+        var email = jq(this).data('review-user-email');
+        var rating = jq(this).data('review-rating');
+        var description = jq(this).data('review-description');
 
-        $("#editReviewId").val(reviewId);
-        $("#editReviewName").val(reviewName);
-        $("#editReviewUserEmail").val(reviewUserEmail);
-        $("input[name='Rating'][value='" + reviewRating + "']").prop("checked", true);
-        $("#editReviewDescription").val(reviewDescription);
+        jq('#editReviewId').val(reviewId);
+        jq('#editReviewName').val(name);
+        jq('#editReviewUserEmail').val(email);
+        jq('#editReviewDescription').val(description);
 
-        jq("#editReviewModal").modal("show");
+        // Debugging log
+        console.log("Selected Rating: ", rating);
+
+        // Check the radio button that matches the rating
+        jq('#editReviewModal input[name="Rating"]').each(function () {
+            if (parseInt(jq(this).val()) === rating) {
+                jq(this).prop('checked', true);
+            }
+        });
+
+        jq('#editReviewModal').modal('show');
     });
+
+    // Debugging: Check change event on rating radio buttons
+    jq('#editReviewModal input[name="Rating"]').change(function () {
+        console.log("New Rating Selected: ", jq(this).val());
+    });
+  
     jq(".editCommentBtn").click(function () {
         var commentId = $(this).data("comment-id");
         var commentName = $(this).data("comment-name");
